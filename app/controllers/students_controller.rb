c4b27974947fdabd35f556c133450c4cf32bca0c
mike <mike@ubuntu.(none)>
2012-03-13 15:52:04 -1000

    final working

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -16,5 +16,22 @@ class StudentsController < ApplicationController
       :last_name => ln, :major => maj)
     @student.save
   end
+  def selectStudents
+    @students = Student.all
+  end
+  def deleteStudent
+    id = params[:student_id]
+    @student = Student.find_by_id(id)
+    if @student.delete
+        redirect_to :controller => "students", :action => "selectStudent"
+    end
+  end
+  def editStudent
+    id = params[:student_id]
+    fn = params[:first_name]
+    ln = params[:last_name]
+    maj = params[:major]
+    @student = Student.update(id,{:first_name => fn, :last_name => ln, :major => maj})
+  end  
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Studemo::Application.routes.draw do
   #get "student/insertStudent"
+  get "students/index"
   match "students/newStudent" => "students#newStudent"
   match "students/createStudent" => "students#createStudent"
-  get "students/index"
+  match "students/selectStudent" => "students#selectStudent"
+  match "students/deleteStudent" => "students#deleteStudent"
+  match "students/editStudent" => "students#editStudent"
 end
